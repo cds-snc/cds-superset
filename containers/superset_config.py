8075d4cf3c1db537c392b3f3cbcfe5e6128a8147
Calvin Rodo <calvin.rodo@cds-snc.ca>
2024-01-15 18:36:26 +0000

    feat: add a simple simple_config
    - in-memory cache
    - no workers for long running queries.

--- containers/superset_config.py
+++ containers/superset_config.py
@@ -7,7 +7,13 @@ logger = logging.getLogger()
 logger.info("Setting up custom config for Superset")
 
 
-
+CACHE_CONFIG = {
+    "CACHE_TYPE": "SupersetMetastoreCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+}
+DATA_CACHE_CONFIG = CACHE_CONFIG
+CELERY_CONFIG = None
 
 
 logger.info("Finished setting up custom config for Superset")
