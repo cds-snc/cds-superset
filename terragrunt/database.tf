15cc5cb4f1dd16c35f270cf7e83b783255157421
Pat Heard <patrick.heard@cds-snc.ca>
2024-05-09 10:22:44 -0400

    fix: rds module name

--- terragrunt/database.tf
+++ terragrunt/database.tf
@@ -46,11 +46,11 @@ resource "aws_ssm_parameter" "superset_database_password" {
 }
 
 import {
-  to = module.rds.aws_security_group_rule.rds_proxy_egress
+  to = module.superset_db.aws_security_group_rule.rds_proxy_egress
   id = "${module.rds.proxy_security_group_id}_egress_tcp_5432_5432_self"
 }
 
 import {
-  to = module.rds.aws_security_group_rule.rds_proxy_ingress
+  to = module.superset_db.aws_security_group_rule.rds_proxy_ingress
   id = "${module.rds.proxy_security_group_id}_ingress_tcp_5432_5432_self"
 }
