da3d457afc6f944abc88e677bb772ba52465e77a
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-14 21:01:32 +0000

    feat: cross-account access to the data lake
    Add a cross-account Glue data catalog that will allow Superset to
    access the data in the data lake.

--- terragrunt/athena_iam.tf
+++ terragrunt/athena_iam.tf
@@ -46,6 +46,7 @@ data "aws_iam_policy_document" "superset_athena_read" {
     actions = [
       "athena:BatchGetNamedQuery",
       "athena:BatchGetQueryExecution",
+      "athena:GetDataCatalog",
       "athena:GetNamedQuery",
       "athena:GetQueryExecution",
       "athena:GetQueryResults",
