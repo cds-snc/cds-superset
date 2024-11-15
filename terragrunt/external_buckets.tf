d86a9e3c76ef593e77d485635b6d23d42a8d3293
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-15 14:53:28 -0500

    chore: remove unused resources (#204)
    Remove resources that are now part of the data lake and no
    longer needed in the Superset account.

--- terragrunt/external_buckets.tf
+++
@@ -1,8 +0,0 @@
-
-data "aws_s3_bucket" "cur_data_extract" {
-  bucket = "713f18dd-9f30-4976-a152-e81d48cf053a"
-}
-
-data "aws_s3_bucket" "tag_data_extract" {
-  bucket = "5bf89a78-1503-4e02-9621-3ac658f558fb"
-}
