faddcf23760be6da3ca69ea9c975686832d8ccfe
Pat Heard <patrick.heard@cds-snc.ca>
2025-05-28 20:40:09 +0000

    feat: switch to Valkey caching engine
    Add a Valkey memcache that we'll use to cache Superset charts
    and dashboards.  The reason for this change is Valkey is ~33%
    cheaper than Redis and acts as drop-in replacement.
    
    This change also slightly increases the cache size in Production
    as we have intermittently seen out-of-memory errors when
    trying to cache results.
    
    The path for applying this change will be as follows:
    
    1. Create the new cluster.
    2. Switch Superset over to new cluster.
    3. Delete existing Redis cluster.

--- terragrunt/aws/redis.tf
+++ terragrunt/aws/redis.tf
@@ -19,3 +19,25 @@ resource "aws_elasticache_subnet_group" "superset" {
   name       = join("-", [local.prefix, "redis-subnet-grp"])
   subnet_ids = module.vpc.private_subnet_ids
 }
+
+resource "aws_elasticache_cluster" "superset_cache" {
+  cluster_id           = "superset-cache-${var.env}"
+  engine               = "valkey"
+  node_type            = var.env == "prod" ? "cache.t2.small" : "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.valkey7"
+  engine_version       = "7"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.superset_cache.name
+
+  security_group_ids = [
+    aws_security_group.superset_redis.id,
+  ]
+
+  tags = local.common_tags
+}
+
+resource "aws_elasticache_subnet_group" "superset_cache" {
+  name       = "superset-cache-${var.env}"
+  subnet_ids = module.vpc.private_subnet_ids
+}
