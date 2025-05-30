f84ead9a24fa335e7c35777a44674b8d2fa3d31f
Pat Heard <patrick.heard@cds-snc.ca>
2025-05-30 13:51:20 -0400

    fix: drop support for TLS 1.2 connections (#432)
    Update the load balancer listener's SSL policy to only
    support TLS 1.3 connections.  This is another finding that
    has come out of the OSCAL scanning work.

--- terragrunt/aws/load_balancer.tf
+++ terragrunt/aws/load_balancer.tf
@@ -59,7 +59,7 @@ resource "aws_lb_listener" "superset" {
   load_balancer_arn = aws_lb.superset.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-FIPS-2023-04"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-FIPS-2023-04"
   certificate_arn   = aws_acm_certificate.superset.arn
 
   default_action {
