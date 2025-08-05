40c8eb4ba316646f09c0f907b4cad24155b8e929
Pat Heard <patrick.heard@cds-snc.ca>
2025-08-05 08:51:28 -0400

    feat: add SES for SMTP server (#497)
    Add SES resources so that Superset can send email.

--- terragrunt/aws/route53.tf
+++ terragrunt/aws/route53.tf
@@ -14,3 +14,44 @@ resource "aws_route53_record" "superset_A" {
     evaluate_target_health = false
   }
 }
+
+# SES domain validation
+resource "aws_route53_record" "superset_ses_verification_TXT" {
+  zone_id = aws_route53_zone.superset.zone_id
+  name    = "_amazonses.${aws_ses_domain_identity.superset.id}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.superset.verification_token]
+}
+
+# Email sending
+resource "aws_route53_record" "superset_spf_TXT" {
+  zone_id = aws_route53_zone.superset.zone_id
+  name    = var.domain
+  type    = "TXT"
+  ttl     = "300"
+  records = [
+    "v=spf1 include:amazonses.com -all"
+  ]
+}
+
+resource "aws_route53_record" "superset_dkim_CNAME" {
+  count   = 3
+  zone_id = aws_route53_zone.superset.zone_id
+  name    = "${element(aws_ses_domain_dkim.superset.dkim_tokens, count.index)}._domainkey.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [
+    "${element(aws_ses_domain_dkim.superset.dkim_tokens, count.index)}.dkim.amazonses.com",
+  ]
+}
+
+resource "aws_route53_record" "superset_dmarc_TXT" {
+  zone_id = aws_route53_zone.superset.zone_id
+  name    = "_dmarc.${var.domain}"
+  type    = "TXT"
+  ttl     = "300"
+  records = [
+    "v=DMARC1; p=reject; sp=reject; pct=100; rua=mailto:CDS.SECURITY-SECURITE.SNC@servicecanada.gc.ca"
+  ]
+}
---
+++ terragrunt/aws/ses.tf
@@ -0,0 +1,58 @@
+#
+# Send email using a SES SMTP server
+#
+resource "aws_ses_domain_identity" "superset" {
+  domain = var.domain
+}
+
+resource "aws_ses_domain_dkim" "superset" {
+  domain = aws_ses_domain_identity.superset.domain
+}
+
+resource "aws_ses_domain_identity_verification" "superset" {
+  domain     = aws_ses_domain_identity.superset.id
+  depends_on = [aws_route53_record.superset_ses_verification_TXT]
+}
+
+resource "aws_iam_user" "superset_send_email" {
+  # checkov:skip=CKV_AWS_273: SES IAM user is required to confirgure SMTP credentials
+  name = "superset_send_email"
+}
+
+resource "aws_iam_group" "superset_send_email" {
+  name = "superset_send_email"
+}
+
+resource "aws_iam_group_membership" "superset_send_email" {
+  name  = aws_iam_user.superset_send_email.name
+  group = aws_iam_group.superset_send_email.name
+  users = [
+    aws_iam_user.superset_send_email.name
+  ]
+}
+
+resource "aws_iam_group_policy_attachment" "superset_send_email" {
+  group      = aws_iam_user.superset_send_email.name
+  policy_arn = aws_iam_policy.superset_send_email.arn
+}
+
+data "aws_iam_policy_document" "superset_send_email" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ses:SendRawEmail"
+    ]
+    resources = [
+      aws_ses_domain_identity.superset.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "superset_send_email" {
+  name   = "superset_send_email"
+  policy = data.aws_iam_policy_document.superset_send_email.json
+}
+
+resource "aws_iam_access_key" "superset_send_email" {
+  user = aws_iam_user.superset_send_email.name
+}
